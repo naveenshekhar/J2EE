c6fca52ea3c7b61115df55f0f8f28e407adead61
naveen <naveen@naveen>
2021-03-18 17:13:37 +0530

    Sended responce from servlet to JSP using RequestDispatcher[Naveen Shekhar]

--- ServletJDBCDemo/src/main/java/com/java/web/GetAlienController.java
+++ ServletJDBCDemo/src/main/java/com/java/web/GetAlienController.java
@@ -2,6 +2,7 @@ package com.java.web;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,35 +15,19 @@ import com.java.web.model.Alien;
  * Servlet implementation class GetAlienController
  */
 public class GetAlienController extends HttpServlet {
-	/*
-	 * private static final long serialVersionUID = 1L;
-	 * 
-	 *//**
-		 * @see HttpServlet#HttpServlet()
-		 *//*
-			 * public GetAlienController() { super(); // TODO Auto-generated constructor
-			 * stub }
-			 */
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		int aid = Integer.parseInt(request.getParameter("aid"));
-		
-		Alien a1 = new  Alien();
-		AlienDao dao = new AlienDao();
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 *//*
-		 * protected void doPost(HttpServletRequest request, HttpServletResponse
-		 * response) throws ServletException, IOException { // TODO Auto-generated
-		 * method stub doGet(request, response); }
-		 */
+		int aid = Integer.parseInt(request.getParameter("aid"));
+
+		AlienDao dao = new AlienDao();
+
+		Alien a1 = dao.getAlien(aid);
+
+//Calling jsp from servlet		
+		RequestDispatcher rd = request.getRequestDispatcher("showAlien.jsp");
+		rd.forward(request, response);
+	}
 
 }
